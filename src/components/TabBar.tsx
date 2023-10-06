476ffc2692ca7b72ec63c456760d7c49bdcd1290
gyf <charon0707@163.com>
2023-10-06 14:06:45 +0800

    Fix:add logo to front page

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -2,10 +2,12 @@ interface TabBarProps {
     children: React.ReactNode;
 }
 
-const TabBar = ({children}: TabBarProps) => {
+const TabBar = ({ children }: TabBarProps) => {
     return (
         <div>
-            <h1>Opendigger Info -- Cli Research Tool</h1>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src="logo2.png" alt="Opendigger Logo" style={{ width: '190px', marginTop: '8px', marginLeft: '4px' }} />
+            </div>
             <hr />
             {children}
         </div>
